b0dc0d501c8d9896ffc0f47bca60e8e4f7b62b1d
Chengpu Wang <Chengpu@gmail.com>
2024-02-02 13:21:21 -0500

    Add exp dump for cpp

--- Cpp/Stat.h
+++ Cpp/Stat.h
@@ -4,6 +4,7 @@ Statistical functions
 
 #include <cmath>
 #include <cstddef>
+#include <limits>
 #include <numeric>
 #include <vector>
 
@@ -13,9 +14,11 @@ namespace var_dbl
 {
 
 struct Stat {
-    double mean;
-    double stddev;
-    unsigned count;
+    double mean = std::numeric_limits<double>::quiet_NaN();
+    double min = std::numeric_limits<double>::max();
+    double max = -std::numeric_limits<double>::max();
+    double stddev = std::numeric_limits<double>::quiet_NaN();
+    unsigned count = 0;
 };
 
 template< class InputIt >
@@ -23,15 +26,18 @@ Stat calcStat(InputIt begin, InputIt end);
 
 struct Histo {
     Stat stat;
-    double range;       // centered at mean and normalized by stddv, may be different from the input
-    unsigned divides;   // per stddev
-    unsigned less;      // less than range count
-    unsigned more;      // more than range count
-    std::vector<std::pair<float, float>> sHisto;    // 2*range*divides buckets around mean
+    float range = 0;       // centered at mean and normalized by stddv, may be different from the input
+    unsigned divides = 0;  // per stddev
+    unsigned less = 0;     // less than range count
+    unsigned more = 0;      // more than range count
+    std::vector<float> sHisto;  // 2*range*divides buckets around stat.mean
+                                // center for each bucket is by calcHistoCenters()
 };
 
+std::vector<float> calcHistoCenters(float range, unsigned divides);
+
 template< class InputIt >
-Histo calcHisto(InputIt begin, InputIt end, double range, unsigned divides, bool normalized=true);
+bool calcHisto(Histo& res, InputIt begin, InputIt end, float range, unsigned divides, bool normalized=true);
 
 
 
@@ -48,28 +54,44 @@ inline Stat calcStat(InputIt begin, InputIt end)
     res.mean = std::accumulate(begin, end, 0.0) / res.count;
     if (res.count <= 1)
         return res;
-    res.stddev = std::accumulate(begin, end, 0, [res](double init, double item){
-            return init + (item - res.mean) * (item - res.mean);
-        }) / (res.count - 1);
-    res.stddev = std::sqrt(res.stddev);
+    res.stddev = 0;
+    for (auto it = begin; it != end; ++it) {
+        if (res.min > *it)
+            res.min = *it;
+        if (res.max < *it)
+            res.max = *it;
+        res.stddev += (*it - res.mean) * (*it - res.mean);
+    }
+    res.stddev = std::sqrt(res.stddev/(res.count - 1));
     return res;
 }
 
 
+inline std::vector<float> calcHistoCenters(float range, unsigned divides)
+{
+    std::vector<float> sRes;
+    if ((range <= 0) || (divides == 0))
+        return sRes;
+    int size = std::round(range * divides);
+    for (int i = -size; i < size; ++i) 
+        sRes.push_back((i + 0.5f)/divides);
+    return sRes;
+}
+
 template< class InputIt >
-inline Histo calcHisto(InputIt begin, InputIt end, double range, unsigned divides, bool normalized) {
-    Histo res;
+inline bool calcHisto(Histo& res, InputIt begin, InputIt end, float range, unsigned divides, bool normalized) {
     res.range = range;
     res.divides = divides;
     res.less = 0;
     res.more = 0;
     res.stat = calcStat(begin, end);
     if (!std::isfinite(res.stat.stddev) || (range <= 0) || (divides == 0))
-        return res;
+        return false;
     int size = std::round(range * divides);
-    res.range = (double) size / divides;
-    for (int i = -size; i < size; ++i) 
-        res.sHisto.push_back({(i + 0.5f)/divides, 0});
+    res.range = (float) size / divides;
+    res.sHisto.clear();
+    res.sHisto.reserve(2*size);
+    res.sHisto.insert(res.sHisto.end(), 2*size, 0);
     for (auto it = begin; it != end; ++it) {
         int idx = std::floor((*it - res.stat.mean) / res.stat.stddev * divides);
         if (idx < -size)
@@ -77,14 +99,14 @@ inline Histo calcHisto(InputIt begin, InputIt end, double range, unsigned divide
         else if (idx >= size)
             ++res.more;
         else 
-            ++res.sHisto[idx + size].second;
+            ++res.sHisto[idx + size];
     }
     const int count = res.stat.count - res.less - res.more;
     if (normalized && (count > 0)) {
         for (int i = -size; i <= size; ++i) 
-            res.sHisto[i + size].second /= count;
+            res.sHisto[i + size] /= count;
     }
-    return res;
+    return true;
 }
 
 
--- Cpp/TestStat.cpp
+++ Cpp/TestStat.cpp
@@ -2,7 +2,6 @@
 #include "Test.h"
 
 #include <iostream>
-#include <utility>
 
 using namespace var_dbl;
 
@@ -11,44 +10,59 @@ void testStat()
     std::vector sData{1,3,2};
     Stat stat = calcStat(sData.begin(), sData.end());
     test::assertEquals(stat.count, 3);
+    test::assertEquals(stat.min, 1);
+    test::assertEquals(stat.max, 3);
     test::assertEquals(stat.mean, 2);
     test::assertEquals(stat.stddev, 1);
 
     sData.pop_back();
     stat = calcStat(sData.begin(), sData.end());
     test::assertEquals(stat.count, 2);
+    test::assertEquals(stat.min, 1);
+    test::assertEquals(stat.max, 3);
     test::assertEquals(stat.mean, 2);
     test::assertEquals(stat.stddev, std::sqrt(2));
 
     sData.pop_back();
     stat = calcStat(sData.begin(), sData.end());
     test::assertEquals(stat.count, 1);
+    test::assertEquals(stat.min, 1);
+    test::assertEquals(stat.max, 1);
     test::assertEquals(stat.mean, 1);
-    test::assertEquals(stat.stddev, std::nan("2"));
+    test::assertEquals(stat.stddev, std::numeric_limits<double>::quiet_NaN());
 
     sData.pop_back();
     stat = calcStat(sData.begin(), sData.end());
+    test::assertEquals(stat.min, std::numeric_limits<double>::max());
+    test::assertEquals(stat.max, -std::numeric_limits<double>::max());
     test::assertEquals(stat.count, 0);
-    test::assertEquals(stat.mean, std::nan("1"));
-    test::assertEquals(stat.stddev, std::nan("2"));
+    test::assertEquals(stat.mean, std::numeric_limits<double>::quiet_NaN());
+    test::assertEquals(stat.stddev, std::numeric_limits<double>::quiet_NaN());
 }
 
 void testHisto()
 {
+    float RANGE = 1.5;
+    unsigned DIVIDES = 2;
     std::vector<int> sData;
     for (int i = -10; i <= 10; ++i) 
         sData.push_back(i);
-    Histo histo = calcHisto(sData.begin(), sData.end(), 1.5, 2);
+    Histo histo;
+    test::assertTrue(calcHisto(histo, sData.begin(), sData.end(), RANGE, DIVIDES));
     test::assertEqual(histo.stat.count, 21);
+    test::assertEqual(histo.stat.min, -10);
+    test::assertEqual(histo.stat.max, 10);
     test::assertEquals(histo.stat.mean, 0);
-    test::assertEquals(histo.stat.stddev, 10/std::sqrt(3)*1.0677078252031309);
-    test::assertEquals(histo.sHisto.size(), 2*3);
+    test::assertEquals(histo.stat.stddev, 10/std::sqrt(3)*1.0747092630102337);
+    test::assertEquals(histo.sHisto.size(), 6);
     test::assertEqual(histo.less, 1);
     test::assertEqual(histo.more, 1);
     double sCenter[] = {-1.25f, -0.75f, -0.25f, 0.25f, 0.75f, 1.25f};
+    std::vector<float> sHistoCenter = calcHistoCenters(RANGE, DIVIDES);
+    test::assertEqual(sHistoCenter.size(), 6);
     for (int i = -3; i < 3; ++i) {
-        test::assertEquals(histo.sHisto[i + 3].first, sCenter[i + 3]);
-        test::assertEquals(histo.sHisto[i + 3].second, ((i == 0)? 4.0f : 3.0f)/19);
+        test::assertEquals(sHistoCenter[i + 3], sCenter[i + 3]);
+        test::assertEquals(histo.sHisto[i + 3], ((i == 0)? 4.0f : 3.0f)/19);
     }
 }
 
